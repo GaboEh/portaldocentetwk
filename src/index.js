1d095004dbacf0b532d06b27758114a9eb59937d
Bayron Bravo <bayron.bravo99@gmail.com>
2022-11-16 17:40:06 -0300

    incluido el cors

--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
-const express = require ('express');
-const mongoose  = require("mongoose");
+const express = require('express');
+const mongoose = require("mongoose");
 require("dotenv").config();
 const userRoutes = require("./routes/usuarios");
 const jorneyRoutes = require("./routes/jornadas");
 const subjectRoutes = require("./routes/ramos");
+const cors = require('cors');
 
 const app = express();
 const port = process.env.PORT || 5000;
+app.use(cors());
 
 //mongobd
 mongoose
-    mongoose.connect(process.env.MONGODB_URI)
-    .then(()=> console.log("Conected"))
+mongoose.connect(process.env.MONGODB_URI)
+    .then(() => console.log("Conected"))
     .catch((error) => console.error(error));
 
 //middleware
@@ -22,7 +24,7 @@ app.use('/api', subjectRoutes);
 
 //rutas
 app.get("/", (req, res) => {
-    res.send ("Bienvenido a la API");
+    res.send("Bienvenido a la API");
 });
 
 app.listen(port, () => console.log('server running on port', port));
